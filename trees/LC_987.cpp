04966e3351e7956e6099414eb72e5d259987f413
Kaneesh Kumar <kaneesh77777@gmail.com>
2026-03-05 22:35:13 +0530

    solved lc hard qn of verticalmorder traversal in tree

---
+++ trees/LC_987.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,map<int,multiset<int>>> m;
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        vector<vector<int>> ans;
+        if(!root)
+        return ans;
+
+        q.push({root,{0,0}});
+
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            TreeNode* node=it.first;
+            int x=it.second.first;
+            int y=it.second.second;
+
+            m[x][y].insert(node->val);
+            if(node->left){
+                q.push({node->left,{x-1,y+1}});
+            }
+
+             if(node->right){
+                q.push({node->right,{x+1,y+1}});
+            }
+        }
+
+        for(auto it:m){
+            vector<int> col;
+            for(auto p:it.second){
+                col.insert(col.end(),p.second.begin(),p.second.end());
+            }
+            ans.push_back(col);
+        }
+        return ans;
+    }
+};
