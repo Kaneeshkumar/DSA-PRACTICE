4a4445a76e53dd9abd12f4a6f7213a52b496f80b
Kaneesh Kumar <kaneesh77777@gmail.com>
2026-04-07 21:40:53 +0530

    solved gfg qn of detecting cycle using BFS

---
+++ graphs/GFG_DETECT_CYCLE.cpp
@@ -0,0 +1,53 @@
+
+bool checkCycle(vector<int> adj[],vector<int>& visited,int i){
+    
+    queue<pair<int,int>> q;
+    
+    visited[i]=1;
+    
+    q.push({i,-1});
+    
+    while(!q.empty()){
+        int curr=q.front().first;
+        int parent=q.front().second;
+        q.pop();
+        
+        for(auto it:adj[curr]){
+            if(!visited[it]){
+                visited[it]=1;
+                q.push({it,curr});
+            }else if(parent!=it)
+            return true;
+        
+        }
+    }
+    return false;
+}
+
+bool isCycle(int edges[][2], int E, int V) {
+    // Code here
+    vector<int> adj[V];
+    vector<int> visited(V,0);
+    
+
+    
+    for(int i=0;i<E;i++){
+        int first=edges[i][0];
+        int second=edges[i][1];
+        
+        adj[first].push_back(second);
+        adj[second].push_back(first);
+        
+        
+    }
+    
+    for(int i=0;i<V;i++){
+        if(!visited[i]){
+           bool b1=checkCycle(adj,visited,i);
+           if(b1)
+           return true;
+        }
+    }
+    return false;
+    
+}
