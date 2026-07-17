363c62523bd22d12c8836be5aa1140482ea0c6f3
Kaneesh Kumar <kaneesh77777@gmail.com>
2026-07-17 14:31:39 +0530

    solved an easy qn of inserting a head element to a doublyLinkedList

--- linked_list/tuf(finding_length_of_LL).cpp
+++ linked_list/tuf(finding_length_of_LL).cpp
@@ -13,34 +13,3 @@ public:
         return cnt;
     }
 };
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    int countNodes(TreeNode* root) {
-        // your code here
-            if(!root)
-            return 0;
-
-
-            int lh=calcLH(root->left);
-            int rh=calcRH(root->right);
-
-            if(lh==rh)
-            return pow(2,lh)-1;
-
-
-
-        
-    }
-};
