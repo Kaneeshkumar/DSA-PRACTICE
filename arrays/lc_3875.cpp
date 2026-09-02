aaf89d52dee4fcd9097668ed34bcbc686b57e592
Kaneesh Kumar <kaneesh77777@gmail.com>
2026-09-02 11:41:42 +0530

    solved a daily qn of finding array of all even or odd numbers based on another array

---
+++ arrays/lc_3875.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // vector<int> ans;
+        // int ceven=0;
+        // int codd=0;
+        // // for(int i=0;i<nums1.size()-1;i++){
+        // //     int currval=nums1[i]-nums1[i+1];
+        // //     if(currval%2==0 && nums1[i]%2==0){
+        // //         iseven=true;
+        // //         ans.push_back(nums1[i]);
+        // //         ans.push_back(nums1[i+1]); 
+        // //     } else if(currval%2==0 && nums1[i]%2!=0){
+        // //         iseven=false;
+        // //         ans.push_back(nums1[i]);
+        // //         ans.push_back(nums1[i+1]); 
+        // //     }
+            
+        // // }
+
+        // for(int val:nums1){
+        //     if(val%2==0)
+        //         ceven++;
+        //     else
+        //         codd++;
+        // }
+        return true;
+    }
+};
